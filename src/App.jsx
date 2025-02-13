a70148567ca305549ea3f2a53d98634a82cf76ce
nazrinshukurova <nezrinnshukurova23>
2025-02-13 17:16:40 +0400

    Even after page refresh,the correct mode will be applied from loaclstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
   const initialMode = localStorage.getItem("mode") || "dark";
   const [mode, setMode] = useState(initialMode);
 
+  useEffect(() => {
+    document.body.classList.add(mode === "dark" ? "dark-mode" : "light-mode");
+  }, []); 
+
   const toggleMode = () => {
     const newMode = mode === "dark" ? "light" : "dark";
     setMode(newMode);
     localStorage.setItem("mode", newMode);
 
-    if (newMode === "light") {
-      document.body.classList.add("light-mode");
-      document.body.classList.remove("dark-mode");
-    } else {
-      document.body.classList.add("dark-mode");
-      document.body.classList.remove("light-mode");
-      
-    }
+    document.body.classList.toggle("dark-mode", newMode === "dark");
+    document.body.classList.toggle("light-mode", newMode === "light");
   };
 
   return (
